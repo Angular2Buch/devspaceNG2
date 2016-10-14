3801c58824a72b537c77814f5142c6e331e6f0d6
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-10-14 12:07:18 +0200

    new component: BookComponent

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Book } from '../shared/book';
+
+
 @Component({
-  selector: 'app-dashboard',
+  selector: 'br-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
+  books: Book[];
+
   constructor() { }
 
   ngOnInit() {
+    this.books = [
+      new Book('123456789', 'AngularJS', 'Old but gold', 4),
+      new Book('098765432', 'Angular 2', 'Alles ist neu!', 5)
+    ];
   }
 
 }
