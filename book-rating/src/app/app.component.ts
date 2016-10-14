f2c2f79b9e54b795546f3e7667d864bd5b76238b
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-10-14 10:22:10 +0200

    first example for change detection

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Developer Open Space! :-)';
+
+  constructor() {
+    setTimeout(() => {
+      this.title = 'changed title!!';
+    }, 3000);
+  }
 }
